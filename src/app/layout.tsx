37d5239a2ae32978d2ab98aba99e8261e432323c
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2025-06-17 13:35:20 +0700

    🍹 Add favicon.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
